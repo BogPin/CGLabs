31214ecb1773175d23d395bd18fccbd62f2c6710
BogPin <bogdanpin2002@gmail.com>
2023-05-24 21:17:46 +0300

    add tests for ppm reader and ppm writer

---
+++ CGLabsTest/PpmReaderTest.cs
@@ -0,0 +1,29 @@
+using System.Text;
+using CGLabs;
+using PpmReaderNS; 
+namespace CGLabsTest;
+
+
+
+
+public class TestReader
+{
+    [Test]
+    public void PpmToPixels_ReturnsCorrectPixelArray()
+    {
+        // Arrange
+        byte[] fileData = Encoding.ASCII.GetBytes("P3\n2 2\n15\n15 0 0\t0 15 0\t\n0 0 15\t15 15 15\t\n");
+        PpmReader ppmReader = new PpmReader();
+
+        // Act
+        Pixel[,] result = ppmReader.ReadImage(fileData);
+
+        // Assert
+        Pixel[,] expected = new Pixel[,]
+        {
+                { new Pixel(255, 0, 0), new Pixel(0, 255, 0) },
+                { new Pixel(0, 0, 255), new Pixel(255, 255, 255) }
+        };
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ CGLabsTest/PpmWriterTest.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using CGLabs;
+using PpmWriterNS;
+
+namespace CGLabsTest;
+
+public class TestWriter
+{
+    [Test]
+    public void PixelsToPpm_ReturnsCorrectPpmByteArray()
+    {
+        // Arrange
+        Pixel[,] pixels = new Pixel[,]
+        {
+                { new Pixel(255, 0, 0), new Pixel(0, 255, 0) },
+                { new Pixel(0, 0, 255), new Pixel(255, 255, 255) }
+        };
+        int maxColorValue = 255;
+        PpmWriter ppmWriter = new PpmWriter();
+
+        // Act
+        byte[] result = ppmWriter.WriteImage(pixels, maxColorValue);
+
+        // Assert
+        byte[] expected = Encoding.ASCII.GetBytes("P3\n2 2\n255\n255 0 0\t0 255 0\t\n0 0 255\t255 255 255\t\n");
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Format_ReturnsPpm()
+    {
+        // Arrange
+        PpmWriter ppmWriter = new PpmWriter();
+
+        // Act
+        string format = ppmWriter.Format;
+
+        // Assert
+        Assert.AreEqual("ppm", format);
+    }
+}
