e05a569903ef8b0d18da61fc4dfb7213bd4599bd
holdennekt <ngryshchak@gmail.com>
2023-05-25 01:04:58 +0300

    add cmd arguments parsing and formats convertion

---
+++ CGLabs/CommandLineArgsParser.cs
@@ -0,0 +1,33 @@
+public static class CommandLineParser
+{
+  public static Dictionary<string, string> ParseOptions(string[] arguments)
+  {
+    var results = new Dictionary<string, string>();
+
+    var lastArg = "";
+
+    foreach (string argument in arguments)
+    {
+      if (string.IsNullOrWhiteSpace(argument))
+      {
+        continue;
+      }
+
+      if (argument.StartsWith("--", StringComparison.Ordinal))
+      {
+        lastArg = argument.Substring(2);
+      }
+      else if (argument.StartsWith("-", StringComparison.Ordinal))
+      {
+        lastArg = argument.Substring(1);
+      }
+      else
+      {
+        results.Add(lastArg, argument);
+        lastArg = "";
+      }
+    }
+
+    return results;
+  }
+}
--- CGLabs/Program.cs
+++ CGLabs/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CGLabs.Factories;
+
+var cmdArgs = CommandLineParser.ParseOptions(args);
+
+var pluginFactory = new PluginFactory("PluginsDLLs");
+
+string maxColorValue = cmdArgs.GetValueOrDefault("maxColor", "255");
+
+var inputFileName = cmdArgs.GetValueOrDefault("source", "");
+if (inputFileName == "")
+  throw new Exception("not found --source argument");
+var inputFormat = inputFileName.Split('.')[1];
+var outputFileName = cmdArgs.GetValueOrDefault("target", "");
+if (outputFileName == "")
+  throw new Exception("not found --target argument");
+var outputFormat = outputFileName.Split('.')[1];
+
+var reader = pluginFactory.GetImageReader(inputFormat);
+var writer = pluginFactory.GetImageWriter(outputFormat);
+
+var pixels = reader.ReadImage(File.ReadAllBytes(inputFileName));
+
+File.WriteAllBytes(outputFileName, writer.WriteImage(pixels, int.Parse(maxColorValue)));
