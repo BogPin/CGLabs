364f7321de378915123f9075139975c3182b8630
BogPin <bogdanpin2002@gmail.com>
2023-05-24 12:53:10 +0300

    add PpmWriter.cs

--- CGLabs/Plugins/Writers/PpmWriter/PpmWriter.cs
+++ CGLabs/Plugins/Writers/PpmWriter/PpmWriter.cs
@@ -1,8 +1,30 @@
 ﻿using CGLabs.Interfaces;
+using CGLabs;
+using System;
+
 
 namespace PpmWriter;
 
 public class PpmWriter : IImageWriter
 {
+    private static byte[] pixelsToPpm(Pixel[,] pixels, int maxColorValue)
+    {
+        double colorCoef = maxColorValue / 255.0;
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"P3\n{pixels.GetLength(1)} {pixels.GetLength(0)}\n{maxColorValue}\n");
 
+        for (int i = 0; i < pixels.GetLength(0); i++)
+        {
+            for (int j = 0; j < pixels.GetLength(1); j++)
+            {
+                Pixel chunk = pixels[i, j];
+                sb.Append($"{Math.Round(chunk.R * colorCoef)} {Math.Round(chunk.G * colorCoef)} {Math.Round(chunk.B * colorCoef)}\t");
+            }
+
+            sb.Append('\n');
+        }
+        return Encoding.ASCII.GetBytes(sb.ToString());
+    }
+
+    public string Format => "ppm";
 }
